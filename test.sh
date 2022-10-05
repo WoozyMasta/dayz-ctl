6bf3f842e45b4104d0bd22c4d793912d45e217b9
Maxim Levchenko <maxim.levchenko@zyfra.com>
2022-10-05 08:02:29 +0300

    feat: update install

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 docker run --rm -ti -e TEST_MODE=true -v "$(pwd):/dayz-ctl" ubuntu:22.04 bash
+curl -sSfL https://raw.githubusercontent.com/WoozyMasta/dayz-ctl/master/install | bash
