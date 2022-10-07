0ba8c2990776790d6ae4da71bfbd03657fcaea12
Woozy Masta <kagbe.leviy@gmail.com>
2022-10-08 02:13:53 +0300

    feat: small fixes

--- test.sh
+++ test.sh
@@ -3,4 +3,8 @@
 docker run --rm -ti -e TEST_MODE=true ubuntu:22.04 bash
 apt update
 apt install curl -y
+# shellcheck disable=SC2016
+printf '%s\n' '#!/usr/bin/env bash' 'printf '\''Steam: %s\n'\'' "${@}"' > /bin/steam
+chmod +x /bin/steam
+mkdir -p "$HOME/.steam/steam/steamapps/common/DayZ"
 curl -sSfL https://raw.githubusercontent.com/WoozyMasta/dayz-ctl/master/install | bash
