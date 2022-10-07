6e1cea62663ea27b0421cf24764815a4df762655
Woozy Masta <kagbe.leviy@gmail.com>
2022-10-08 00:25:47 +0300

    initial

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker run --rm -ti -e TEST_MODE=true ubuntu:22.04 bash
+apt update
+apt install wget -y
+wget -O- https://raw.githubusercontent.com/WoozyMasta/dayz-ctl/master/install | bash
