5138a14b9ba4deab2a7a7a07339add7b2beb0f13
Woozy Masta <kagbe.leviy@gmail.com>
2022-10-08 05:40:50 +0300

    feat: polishing a lot of small problems

--- test.sh
+++ test.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-docker run --rm -ti -e TEST_MODE=true ubuntu:22.04 bash
-apt update
-apt install curl -y
-# shellcheck disable=SC2016
-printf '%s\n' '#!/usr/bin/env bash' 'printf '\''Steam: %s\n'\'' "${@}"' > /bin/steam
-chmod +x /bin/steam
-mkdir -p "$HOME/.steam/steam/steamapps/common/DayZ"
-curl -sSfL https://raw.githubusercontent.com/WoozyMasta/dayz-ctl/master/install | bash
+command -v docker &>/dev/null && cri=docker
+command -v podman &>/dev/null && cri=podman
+[ -z $cri ] && { >&2 echo 'Need Container Runtime for run tests'; exit 1; }
+
+images=(
+  docker.io/debian:bookworm
+  docker.io/debian:bullseye
+  docker.io/debian:buster
+  docker.io/ubuntu:18.04
+  docker.io/ubuntu:20.04
+  docker.io/ubuntu:22.04
+  docker.io/fedora:latest
+  docker.io/centos:7
+  quay.io/centos/centos:stream9
+  docker.io/alpine:latest
+  docker.io/alpine:edge
+  docker.io/archlinux:latest
+  registry.opensuse.org/opensuse/leap:latest
+)
+
+mapfile -t choosed_images < <(gum choose --no-limit "${images[@]}")
+
+for image in "${choosed_images[@]}"; do
+  $cri run --rm -ti -v "$(pwd):/src/" -e TEST_MODE=true "$image" /src/install
+done
